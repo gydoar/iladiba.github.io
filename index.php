6825f7dde19e94760eb3d7e7cb5dd02018358c03
Andres Vega <gydoar@hotmail.com>
2016-07-18 15:12:48 -0500

    terminando el slider

--- index.php
+++ index.php
@@ -7,16 +7,11 @@
 			<!-- Slideshow -->
 			<div class="eight columns">
 				<div id="slider">
-					<img src="images/home/slider/1.jpg" data-src-2x="images/home/slider/1@2x.jpg" title="Baloons" alt="This is the caption content" />
-					<img data-src="images/home/slider/2.jpg" data-src-2x="images/home/slider/2@2x.jpg" src="" title="Skateboard" alt="Captions <em>can</em> contain <strong>HTML</strong>" />
-					<img data-src="images/home/slider/3.jpg" data-src-2x="images/home/slider/3@2x.jpg" src="" alt="This will be overridden" data-caption="#caption-3" />
-					<img data-src="images/home/slider/4.jpg" data-src-2x="images/home/slider/4@2x.jpg" src="" title="A title without a caption" alt="" />
-					<img data-src="images/home/slider/3.jpg" data-src-2x="images/home/slider/4@2x.jpg" src="" title="Silly?" alt="A really really really" />		
-				</div>
+					 <a href="#"><img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" /></a>
 
-				<div id="caption-3" style="display:none;">
-					<h3>External Caption</h3>
-					<p>When using <a href="#">HTML in captions</a> it might be better to use an external caption.</p>
+					<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
+
+					<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
 				</div>
 			</div>
 
