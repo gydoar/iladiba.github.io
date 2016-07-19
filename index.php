7d1e630aff2a936bf4c66b99e5e487e401ed3c80
Andres Vega <gydoar@hotmail.com>
2016-07-19 11:16:22 -0500

    Terminando lo mas destacado

--- index.php
+++ index.php
@@ -1,41 +1,89 @@
 
 <!-- Header principal -->
 <?php include("header.php"); ?>
+	
+	<div class="home">
+	
+		<!-- Slidershow -->
+		<div class="container">
+			<div class="row">
+				<!-- Slideshow -->
+				<div class="eight columns">
+					<div id="slider">
+						 <a href="#"><img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" /></a>
 
-	<div class="container">
-		<div class="row">
-			<!-- Slideshow -->
-			<div class="eight columns">
-				<div id="slider">
-					 <a href="#"><img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" /></a>
+						<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
 
-					<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
-
-					<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
+						<img src="images/home/slider/slide1.jpg" data-src-2x="images/home/slider/slide1.png" title="Abril 25 2016" alt="FDA alerta sobre arritmias con azitromicina" />
+					</div>
 				</div>
-			</div>
 
-			<!-- Articulos del slideshow -->
-			<div class="four columns art__slide__home">
-				<div class="">
-					<ul>
-						<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
-						<a href="#">Ver más ></a>
+				<!-- Articulos del slideshow -->
+				<div class="four columns art__slide__home">
+					<div class="">
+						<ul>
+							<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
+							<a class="boton_vermas" href="#">Ver más ></a>
 
-						<div class="puntos_suspensivos"></div>
-							
-						<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
-						<a href="#">Ver más ></a>
+							<div class="puntos_suspensivos"></div>
+								
+							<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
+							<a class="boton_vermas" href="#">Ver más ></a>
 
-						<div class="puntos_suspensivos"></div>
+							<div class="puntos_suspensivos"></div>
 
-						<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
-						<a href="#">Ver más ></a>
+							<li><p>Guía de la American Gastronterological Association (AGA) sobre manejo…</p></li>
+							<a class="boton_vermas" href="#">Ver más ></a>
 
-					</ul>
+						</ul>
+					</div>
 				</div>
 			</div>
 		</div>
+
+		<!-- Lo mas destacado -->
+		<div class="container sector-2">
+			<div class="row">
+				<div class="nine columns lo__mas__destacado">
+					<h3>LO MÁS DESTACADO</h3> <div class="line__destacado__home"></div>
+					<!-- Cajas destacado -->
+					<div class="row cajas__destacado">
+						<div class="four columns center">
+							<a href="#"><img width="190px" src="images/home/destacado1.png" alt=""></a>
+							<h2>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a><br>
+
+							<h2><br>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a>
+						</div>
+						<div class="four columns center">
+							<a href="#"><img width="190px" src="images/home/destacado1.png" alt=""></a>
+							<h2>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a><br>
+
+							<h2><br>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a>
+						</div>
+						<div class="four columns center">
+							<a href="#"><img width="190px" src="images/home/destacado1.png" alt=""></a>
+							<h2>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a><br>
+
+							<h2><br>Las guías son especialmente aplicables si el problema es molesto...</h2>
+							<a class="boton_vermas boton_2" href="#">Ver más ></a>
+						</div>
+					</div>
+				</div>
+				<!-- Revista home -->
+				<div class="three columns revista__home">
+					<h3>Última Edición Revisada</h3>
+					<a href="#"><img width="170px" src="images/home/revista_iladiba.png" alt="Revista"></a><br>
+					<span>Marzo - Abril 2016</span><br>
+					<a class="boton_vermas boton_3" href="#">Ver más ></a>
+				</div>
+			</div>
+		</div>
+
 	</div>
 
 <!-- Footer principal -->
